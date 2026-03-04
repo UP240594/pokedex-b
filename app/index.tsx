9234b07fd34c55d148b13350789fbdd4737495d7
UP240594 <up240594@alumnos.upa.edu.mx>
2026-03-04 17:40:25 -0600

    show pokemon on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [results, setResults] = useState<any[]>([]);
+
   useEffect(() => {
     //Siempre se va a reproducir cuando el componente entre por primera vez
     console.log("Entre en pantalla");
@@ -9,12 +11,13 @@ export default function Index() {
   }, []);
 
   const getPokemons = async () => {
-    const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
     const response = await fetch(URL, {
       method: "GET",
     }); //Esto es un request
     console.log(response);
     const data = await response.json();
+    setResults(data.results);
     console.log(data);
     //guarda esto en memoria
   }; //esto es una funcion flecha,
@@ -26,6 +29,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      {results.map((pokemon) => (
+        <Text key={pokemon.name}> {pokemon.name} </Text>
+      ))}
       <Text>Oliolioliolio</Text>
     </View>
   );
